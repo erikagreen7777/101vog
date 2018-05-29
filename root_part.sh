99116023e9dc5c2a9588a7f3cb438ee200dd6eb0
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-05-28 21:38:57 -0400

    upperase

--- root_part.sh
+++ root_part.sh
@@ -6,23 +6,23 @@
 
 cat >> /etc/krb5.conf << EOF
 [libdefaults]
-    default_realm = le-101.fr
+    default_realm = LE-101.FR
     forwardable = true
     proxiable = true
     dns_lookup_kdc = no
     dns_lookup_realm = no
     allow_weak_crypto = true
 [realms]
-    le-101.fr = {
-        kdc = kdc.le-101.fr
-        admin_server = kdc.le-101.fr
+    LE-101.FR = {
+        kdc = kerberos.le-101.fr
+        admin_server = kerberos.le-101.fr
         default_domain = le-101.fr
         default_lifetime = 7d
         ticket_lifetime = 7d
     }
 [domain_realm]
-    .le-101.fr = le-101.fr 
-    le-101.fr = le-101.fr 
+    .le-101.fr = LE-101.FR 
+    le-101.fr = LE-101.FR 
 EOF
 
 ### We set up the ssh config file to access to 42's git server (vogsphere)
